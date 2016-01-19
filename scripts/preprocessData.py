f3f4241012747775ccca65e9fbb7c35fc72c3747
huergasi <huergasi@mskcc.org>
2016-01-19 14:16:49 -0500

    float32 for operations v3

--- scripts/preprocessData.py
+++ scripts/preprocessData.py
@@ -18,13 +18,13 @@ def parsefile(filename, targetoffset):
     i = 0
     for line in file:
         row = np.array(line.split(','))
-        current = np.take(row, indices).astype(np.float)
+        current = np.take(row, indices).astype(np.float32)
         if start == 0:
-            previous = np.take(row, indices).astype(np.float)
-            previous_volume = np.take(row, volume_index).astype(np.float)
+            previous = np.take(row, indices).astype(np.float32)
+            previous_volume = np.take(row, volume_index).astype(np.float32)
             start+=1
         else:
-            current_volume = np.take(row, volume_index).astype(np.float)
+            current_volume = np.take(row, volume_index).astype(np.float32)
             sample = np.empty((1,current.size + 1))
             for index in np.arange(current.size):
                 sample[0][index] = ((current[index] - previous[index]) / previous[index]) * 100
@@ -44,28 +44,28 @@ def parse_and_normalize_file(filename, targetoffset, mu, sigma, y_max, y_min):
     file = open(filename, 'r')
     start = 0
     previous = []
-    x= np.empty([1,13], dtype=t.config.floatX)
-    y= np.empty([1], dtype=t.config.floatX)
+    x= np.empty([1,13], dtype=np.float32)
+    y= np.empty([1], dtype=np.float32)
     i = 0
     for line in file:
         row = np.array(line.split(','))
-        current = np.take(row, indices).astype(np.float)
+        current = np.take(row, indices).astype(np.float32)
         if start == 0:
-            previous = np.take(row, indices).astype(np.float)
-            previous_volume = np.take(row, volume_index).astype(np.float)
+            previous = np.take(row, indices).astype(np.float32)
+            previous_volume = np.take(row, volume_index).astype(np.float32)
             start+=1
         else:
-            current_volume = np.take(row, volume_index).astype(np.float)
+            current_volume = np.take(row, volume_index).astype(np.float32)
             sample = np.empty((1,current.size + 1))
             for index in np.arange(current.size):
                 sample[0][index] = ((current[index] - previous[index]) / previous[index]) * 100
             sample[0][current.size] = current_volume
             previous = current
             if i  < (original_data.size / 13) - targetoffset:
-                sample = (sample - mu )/ sigma
+                sample = ((sample - mu )/ sigma).astype(np.float32)
                 x = np.vstack((x, sample))
-                current_y = ((original_data[(i + targetoffset)*13] - current[0]) / current[0]) * 100
-                current_y = (2* (current_y - y_min) / (y_max-y_min)) -1
+                current_y = (((original_data[(i + targetoffset)*13] - current[0]) / current[0]) * 100).astype(np.float32)
+                current_y = ((2 * (current_y - y_min) / (y_max-y_min)) - 1).astype(np.float32)
                 y = np.vstack((y,current_y))
             i+=1
     x = np.delete(x, 0, 0)
@@ -77,8 +77,8 @@ def get_mu_sigma_y(filepath, targetoffset):
     files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
     alldata = []
     ally=[]
-    mu = np.zeros((13,), dtype=np.float64)
-    sigma = np.zeros((13,), dtype=np.float64)
+    mu = np.zeros((13,), dtype=np.float32)
+    sigma = np.zeros((13,), dtype=np.float32)
     y_max = 0
     y_min = 0
     for f in files:
@@ -124,8 +124,8 @@ def getoriginaldata(filename):
     return data
 
 def getdata():
-    path_training=r"C:\Users\huergasi\MLCode\myCode\data\ib\30sec\training"
-    path_testing=r"C:\Users\huergasi\MLCode\myCode\data\ib\30sec\testing"
+    path_training=r"C:\Users\huergasi\MLCode\myCode\data\ib\30sec\training_sample"
+    path_testing=r"C:\Users\huergasi\MLCode\myCode\data\ib\30sec\testing_sample"
     path_validation=r"C:\Users\huergasi\MLCode\myCode\data\ib\30sec\validation_sample"
 
     training_files= [f for f in listdir(path_training) if isfile(join(path_training, f))]
